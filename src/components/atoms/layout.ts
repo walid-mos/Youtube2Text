f543c210c900ccfa10f4e418a20884ed177080e4
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2023-07-05 14:57:48 +0200

    Quit dropdown when clicked outside

--- src/components/atoms/layout.ts
+++ src/components/atoms/layout.ts
@@ -2,3 +2,7 @@ import { atom } from 'jotai'
 
 export const isMenuOpenAtom = atom(false)
 export const isLanguageMenuOpenAtom = atom(false)
+export const languagesMenuIndexAtom = atom(-1)
+export const isLoadedLanguageMenuAtom = atom(
+	(get) => get(languagesMenuIndexAtom) >= 0,
+)
--- src/components/layout/Header/LanguageSwitcher.tsx
+++ src/components/layout/Header/LanguageSwitcher.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useCookies } from 'react-cookie'
 import { useRouter, usePathname } from 'next/navigation'
-import { useAtomValue, useSetAtom } from 'jotai'
+import { useAtom, useAtomValue } from 'jotai'
 
-import { isLanguageMenuOpenAtom } from '@/components/atoms/layout'
+import { isLanguageMenuOpenAtom, languagesMenuIndexAtom, isLoadedLanguageMenuAtom } from '@/components/atoms/layout'
 import { ChevronDownIcon, FRFlagIcon, USFlagIcon } from '@/components/icons'
 import StepCheck from '@/components/icons/svg/StepCheck'
 
@@ -33,35 +33,50 @@ const languages: LanguagesType = [
 ]
 
 const LanguageSwitcher = () => {
-	const isLanguageMenuOpen = useAtomValue(isLanguageMenuOpenAtom)
-	const setIsLanguageMenuOpen = useSetAtom(isLanguageMenuOpenAtom)
+	const [isLanguageMenuOpen, setIsLanguageMenuOpen] = useAtom(isLanguageMenuOpenAtom)
+	const isLoadedLanguageMenuIndex = useAtomValue(isLoadedLanguageMenuAtom)
+	const [languageMenuIndex, setLanguageMenuIndex] = useAtom(languagesMenuIndexAtom)
 
 	const router = useRouter()
 	const pathname = usePathname()
 	const [cookies, setCookie] = useCookies([LOCALE_COOKIE_NAME])
+	const languageDropdownRef = useRef(null)
 
 	const switchLanguage = (code: LOCALES_TYPE) => {
+		setLanguageMenuIndex(languages.findIndex((lang) => lang.code === code))
+		setIsLanguageMenuOpen(false)
 		setCookie(LOCALE_COOKIE_NAME, code)
 		router.push(generateLocalePath(code, pathname))
-		setIsLanguageMenuOpen(false)
 	}
 
-	const actualLanguageIndex = languages.findIndex(({ code }) => code === cookies.i18next)
+	// Set the language menu index to the current language
+	useEffect(() => {
+		setLanguageMenuIndex(languages.findIndex((lang) => lang.code === cookies[LOCALE_COOKIE_NAME]))
+	}, [])
 
-	// useEffect(() => {
-	// 	// Close the language menu when the user clicks outside of it
-	// }, [])
+	// Close the language menu when the user clicks outside of it
+	useEffect(() => {
+		if (!isLanguageMenuOpen) return undefined
+		function handleClick(event: MouseEvent) {
+			if (languageDropdownRef.current && !(languageDropdownRef.current as HTMLElement).contains(event.target as Node)) {
+				setIsLanguageMenuOpen(false)
+			}
+		}
+		document.addEventListener('click', handleClick)
+		// clean up
+		return () => document.removeEventListener('click', handleClick)
+	}, [isLanguageMenuOpen])
 
 	return (
 		<div className="relative hidden pb-5 md:block">
 			<button type="button" className="flex items-center py-2 pl-5 pr-3 text-gray-500 bg-white rounded shadow focus:outline-none" onClick={() => setIsLanguageMenuOpen((get) => !get)}>
 				<span className="pr-4">
-					{languages[actualLanguageIndex].icon || <LoadingAnimated className="rounded-full text-gray-700/20 dark:text-gray-400/30 fill-white/80" />}
+					{isLoadedLanguageMenuIndex ? languages[languageMenuIndex].icon : <LoadingAnimated className="rounded-full text-gray-700/20 dark:text-gray-400/30 fill-white/80" />}
 				</span>
 				<ChevronDownIcon size="s" />
 			</button>
 			{isLanguageMenuOpen && (
-				<div className="absolute top-0 right-0 z-30 w-48 min-w-full mt-12 text-sm text-gray-700 bg-white rounded shadow-md">
+				<div className="absolute top-0 right-0 z-30 w-48 min-w-full mt-12 text-sm text-gray-700 bg-white rounded shadow-md" ref={languageDropdownRef}>
 					<span className="absolute top-0 right-0 w-3 h-3 mr-3 -mt-1 transform rotate-45 bg-white" />
 					<div className="relative z-10 w-full overflow-auto bg-white rounded">
 						<ul className="list-reset">
